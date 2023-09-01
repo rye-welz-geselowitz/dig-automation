4266e52db63540e6790df9d3982e9b27bd624282
Rye Welz Geselowitz <ryewelzgeselowitz@Ryes-MacBook-Pro.local>
2023-08-31 21:22:58 -0400

    check for congratulations text

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import os
 from selenium.webdriver.chrome.service import Service
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -34,7 +35,7 @@ FIELDS_TO_DIG_LABELS = {
 DIG_LABELS_TO_FIELDS = {v:k for k,v in FIELDS_TO_DIG_LABELS.items()}
 
 
-SMS_OPT_IN_OPTIONS = {'True': 'Yes', 'False': 'No'}
+SMS_OPT_IN_OPTIONS = {'True', 'False'}
 
 
 def fill_out_form(data):
@@ -45,28 +46,37 @@ def fill_out_form(data):
     chrome_options.add_argument("--no-sandbox")
     service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.get(FORM_URL)
-    form_fields = driver.find_elements(By.CLASS_NAME, "sharedFormField")
-    for form_field in form_fields:
-        title_element = form_field.find_element(By.CLASS_NAME, 'title')
-        key = title_element.text.replace('*', '').strip()
-        if key in DIG_LABELS_TO_FIELDS:
-            value = data[DIG_LABELS_TO_FIELDS[key]]
-            input_element = form_field.find_element(By.TAG_NAME, 'input')
-            input_element.send_keys(value)
-        elif 'opt out of sms' in key.lower():
-            if data['sms_opt_in'] != 'True':
-                cell_container = form_field.find_element(By.CLASS_NAME, 'border-thick') # super brittle lol, find a better way?
-                cell_container.click()
+    try:
+        driver.get(FORM_URL)
+        form_fields = driver.find_elements(By.CLASS_NAME, "sharedFormField")
+        for form_field in form_fields:
+            title_element = form_field.find_element(By.CLASS_NAME, 'title')
+            key = title_element.text.replace('*', '').strip()
+            if key in DIG_LABELS_TO_FIELDS:
+                value = data[DIG_LABELS_TO_FIELDS[key]]
+                input_element = form_field.find_element(By.TAG_NAME, 'input')
+                input_element.send_keys(value)
+            elif 'opt out of sms' in key.lower():
+                if data['sms_opt_in'] != 'True':
+                    cell_container = form_field.find_element(By.CLASS_NAME, 'border-thick') # super brittle lol, find a better way?
+                    cell_container.click()
 
-
-    if DO_SUBMIT:
-        submit_button_element = driver.find_element(By.CLASS_NAME, "submitButton") 
-        submit_button_element.click()
+        if DO_SUBMIT:
+            submit_button_element = driver.find_element(By.CLASS_NAME, "submitButton") 
+            submit_button_element.click()
+            message_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "message"))
+            )
+            print(message_element)
+            assert 'congratulations' in message_element.text.lower()
+        else:
+            print('Skipping submit!')
+    except Exception as e:
+        driver.quit()
+        raise e
     else:
-        print('Skipping submit!')
-
-
+        driver.quit()
+    
 
 
 @auth.verify_password
